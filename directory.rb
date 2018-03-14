c5fcf1c7643caf2e479fec941e16bd1b43d74e2b
Vytis Vadoklis <vytis.vadoklis@outlook.com>
2018-03-14 19:58:01 +0000

    Exercise 6

--- directory.rb
+++ directory.rb
@@ -1,6 +1,6 @@
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  puts "Please enter the names of the students".center(75)
+  puts "To finish, just hit return twice".center(75)
   # create an empty array
   students = []
   # get the first name
@@ -9,7 +9,7 @@ def input_students
   while !name.empty? do
     # add the student hash to the array
     students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    puts "Now we have #{students.count} students".center(75)
     # get another name from the user
     name = gets.chomp
   end
@@ -18,20 +18,18 @@ def input_students
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The students of Villains Academy".center(75)
+  puts "--------------------------------".center(75)
 end
 
 def print(students)
-  counter = 0
-  while counter < students.length
-    puts "#{students[counter][:name]} (#{students[counter][:cohort]} cohort)"
-    counter += 1
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)".center(75)
   end
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  puts "Overall, we have #{students.count} great students".center(75)
 end
 
 students = input_students
