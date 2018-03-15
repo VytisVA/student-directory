8f157c6b4e935ec3a6ebe063d71ee19eb21c1003
Vytis Vadoklis <vytis.vadoklis@outlook.com>
2018-03-15 14:14:08 +0000

    Exercise 4

--- directory.rb
+++ directory.rb
@@ -5,7 +5,7 @@ def print_menu
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
   puts "4. Load the list from students.csv"
-  puts "9. Exit"
+  puts "5. Exit"
 end
 
 def interactive_menu
@@ -18,27 +18,32 @@ end
 def process(selection)
   case selection
   when "1"
+    puts "Input the students option selected".center(75)
     input_students
+    
   when "2"
+    puts "Show the students option selected".center(75)
     show_students
-  when "9"
+  when "5"
     exit
   when "3"
-    save_students
+    puts "Save the list option selected".center(75)
+    save_students  
   when "4"
+    puts "Load the list option selected".center(75)
     load_students
   else
-    puts "I don't know what you meant, try again"
+    puts "I don't know what you meant, try again".center(75)
   end
 end
 
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  puts "Please enter the names of the students".center(75)
+  puts "To finish, just hit return twice".center(75)
   name = STDIN.gets.chomp
   while !name.empty? do
     @students << {name: name, cohort: :november}
-    puts "Now we have #{@students.count} students"
+    puts "Now we have #{@students.count} students".center(75)
     name = STDIN.gets.chomp
   end
 end
@@ -50,18 +55,18 @@ def show_students
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The students of Villains Academy".center(75)
+  puts "--------------------------------".center(75)
 end
 
 def print_student_list
   @students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    puts "#{student[:name]} (#{student[:cohort]} cohort)".center(75)
   end
 end
 
 def print_footer
-  puts "Overall, we have #{@students.count} great students"
+  puts "Overall, we have #{@students.count} great students".center(75)
 end
 
 def save_students
@@ -88,9 +93,9 @@ def try_load_students
   return if filename.nil?
   if File.exists?(filename)
     load_students(filename)
-     puts "Loaded #{@students.count} from #{filename}"
+     puts "Loaded #{@students.count} from #{filename}".center(75)
   else
-    puts "Sorry, #{filename} doesn't exist."
+    puts "Sorry, #{filename} doesn't exist.".center(75)
     exit
   end
 end
